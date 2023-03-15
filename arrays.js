24a7b5cd64a2ef45303f6d3707a0b0f7ba20c676
Gentlemannerss <damonvanbergen@gmail.com>
2023-03-15 11:58:16 +0100

    uitwerkingen

--- arrays.js
+++ arrays.js
@@ -2,17 +2,27 @@
 // 1. Declareer een variabele en sla daar een array met vier jaartallen (zoals 2021) in op. Log dit in de terminal.
 // ==========================================
 
+const years = [1995, 2000, 2010, 2020];
+console.log(years[0]);
+console.log(years[1]);
+console.log(years[2]);
+console.log(years[3]);
+
 
 // ==========================================
 // 2. Declareer een variabele en sla daar een array met drie van jouw hobbies (zoals muziek luisteren en koekjes eten) in op. Log dit in de terminal.
 // ==========================================
 
+const hobbies = ["game", "muziek luisteren", "voedsel eten"];
+console.log(hobbies);
 
 // ==========================================
 // 3a. Declareer een variabele met daarin een array met de waardes 3, 4, 5, 7 en 2
 // 3b. Zorg er nu voor dat je de eerste waarde uit die array in de terminal logt             // geeft 3
 // ==========================================
 
+const numbers = [3, 4, 5, 7, 2];
+console.log(numbers[0]);
 
 // ==========================================
 // 4a. Declareer een variabele met daarin een array met de waardes groen, geel, rood, paars, blauw en oranje.
@@ -21,6 +31,9 @@
 // 4c. Zorg ervoor dat je de tweede waarde uit bovenstaande array in de terminal logt       // geeft geel
 // ==========================================
 
+const colors = ["groen", "geel", "rood", "paars", "blauw", "oranje"];
+console.log(colors[4]);
+console.log(colors[1]);
 
 // ==========================================
 // 5a. Declareer een variabele met daarin een array met de waardes 21, 22, 23, 25, 25 en log dit in de terminal
@@ -28,10 +41,19 @@
 // 5c. Log de array in de terminal                                                          // geeft [ 21, 22, 23, 24, 25 ]
 // ==========================================
 
+const numbers2 = [1, 22, 23, 25, 25];
+console.log(numbers2);
+numbers2[3] = 24;
+console.log(numbers2);
 
 // ==========================================
 // 6a. Declareer een variabele met daarin een array met de waardes bladerdeeg, knoflook, spinazie
 // 6b. Overschrijf daarna de waarde bladerdeeg met lasagne bladen (je mag de declaratie op de vorige regel niet aanpassen).
-// 6c. Log de de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
+// 6c. Log de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
 // 6d. Log de lengte van de array in de terminal                                            // geeft 3
 // ==========================================
+
+const ingredients = ["bladerdeeg", "knoflook", "spinazie"];
+ingredients[0] = "lasagne bladen";
+console.log(ingredients);
+console.log(ingredients.length);
--- objects.js
+++ objects.js
@@ -2,11 +2,19 @@
 // Maak een object met daarin jouw voornaam (string), achternaam (string), leeftijd (getal) en geboortedatum (date object). De datum hoeft niet geformat te worden.
 // Log dit in de terminal.
 
+const fullName = {
+  firstName : "Damon",
+  lastName : "van Bergen",
+  age : 27,
+  birthDate : 22_09_1995,
+}
+console.log(fullName);
 
 // ===  Opdracht 2 ===
 // Zorg ervoor dat de volgende string in de terminal wordt gelogd:
 // "Hoi! Ik ben <voornaam>, ik ben <leeftijd> jaar oud."
 
+console.log("Hoi! Ik ben ", fullName.firstName, ", ik ben ", fullName.age, " jaar oud.");
 
 // === Opdracht 3: bekijk het volgende object ===
 
@@ -21,15 +29,26 @@ const product = {
 
 // 3a. Log de naam van de serie in de terminal.
 
+console.log(product.series);
+
 // 3b. Overschrijf de oude waarde van de property color ("creme") naar "roze". Log het object in de terminal om te checken of het gelukt is.
+
+product.color = "roze";
+console.log(product.color);
 
 // 3c. Log de tweede materiaalsoort ("kunststof") in de terminal
 
+console.log(product.material[1]);
+
 // 1d. Log de afmetingen (b-h-d) in de terminal
 
+console.log(product["b-h-d"]);
 
 // === Opdracht 4 ===
 // Declareer een variabele met daarin een string die het product beschrijft. Gebruik hiervoor de properties productName en material uit het object.
+
+console.log("Dit is de ", product.productName, " een high class koker gemaakt van premium materialen zoals ", product.material[0], " & ", product.material[1]);
+
 // ---- Verwachte uitkomst:
 // "De Smeg Waterkoker is gemaakt van roestvrijstaal en kunststof"
 
